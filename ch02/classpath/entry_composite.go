6daf203ead7ad24469a93dc5d5577589642921be
Daniel49zhu <zhujiacheng94@163.com>
2018-12-09 22:20:16 +0800

    ch02

--- ch02/classpath/entry_composite.go
+++ ch02/classpath/entry_composite.go
@@ -1,18 +1,18 @@
 package classpath
 
-import (
-	"errors"
-	"strings"
-)
+import "errors"
+import "strings"
 
 type CompositeEntry []Entry
 
 func newCompositeEntry(pathList string) CompositeEntry {
 	compositeEntry := []Entry{}
+
 	for _, path := range strings.Split(pathList, pathListSeparator) {
 		entry := newEntry(path)
 		compositeEntry = append(compositeEntry, entry)
 	}
+
 	return compositeEntry
 }
 
